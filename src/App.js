564538b989555ccc88c641b03c02da442c5368ed
collinsvfx <collinsvfx@gmail.com>
2022-12-18 03:53:51 +0100

    Created a card component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Header from "./components/Header";
 import Hero from "./components/Hero";
+import Card from "./components/Card";
 
 export default function App() {
   return (
@@ -8,6 +9,7 @@ export default function App() {
       <div className="for-app">
         <Header />
         <Hero />
+        <Card />
       </div>
     </div>
   );
