a4a1a6c4edea8e0503c40f2a60c28bb93be8129d
collinsvfx <collinsvfx@gmail.com>
2023-01-11 07:05:37 +0100

    integrated props on Card component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import Header from "./components/Header";
 import Hero from "./components/Hero";
 import Card from "./components/Card";
+import ProductPhoto from "./images/image 12.png";
 
 export default function App() {
   return (
@@ -9,7 +10,14 @@ export default function App() {
       <div className="for-app">
         <Header />
         <Hero />
-        <Card />
+        <Card
+          img={ProductPhoto}
+          rating="5.0"
+          reviewCount={6}
+          country="Nigeria"
+          title="Life Lessons with Katie Zaferes"
+          price={136}
+        />
       </div>
     </div>
   );
--- src/components/Card.js
+++ src/components/Card.js
@@ -1,47 +1,27 @@
 import React from "react";
-import ProductPhoto from "../images/image 12.png";
 import StarImg from "../images/Star 1.png";
 
-/*
-Challenge: Build the Card component
-For now, hard-code in the data (like 
-the rating, title, price, etc.)
-
-Notes:
-- Only render 1 instance (I already did this for you)
-- The star icon and photo (katie-zaferes.png) are in the images 
-  folder for your use
-- Make sure to include:
-    - image
-    - star icon (star.png), rating, and review count
-    - title
-    - cost/person
-- The main purpose of this challenge is to show you where our limitations
-  currently are, so don't worry about the fact that you're hard-coding all
-  this data into the component.
-*/
-
-export default function Card() {
+export default function Card(props) {
   return (
     <div className="card-holder">
       <div className="card-img">
-        <img alt="" src={ProductPhoto} />
+        <img alt="" src={props.img} />
       </div>
       <div className="the-ratings">
         <img alt="" src={StarImg} />
         <div className="rating-text">
           <p className="fivepz">
-            5.0{" "}
+            {props.rating}
             <span className="f-stop">
-              (6) <span className="stopp"> • </span>USA
+              ({props.reviewCount}) <span className="stopp"> • </span>USA
             </span>
           </p>
         </div>
       </div>
       <div className="prodName-title">
-        <h3>Life lessons with Katie Zaferes</h3>
+        <h3>{props.title}</h3>
         <p>
-          <span className="price">From $136</span> /{" "}
+          <span className="price">From ${props.price}</span> /{" "}
           <span className="person">person</span>
         </p>
       </div>
